6ad18a86d4c6756a97da17342aa5eb131e2dd7c3
Camilazinha <caamiladepaula03@gmail.com>
2024-10-08 08:28:59 -0300

    Adição de JS Bootstrap

--- doapego-react/src/index.js
+++ doapego-react/src/index.js
@@ -6,6 +6,7 @@ import './styles/forms.css';
 import './styles/login.css';
 import './styles/layout.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
