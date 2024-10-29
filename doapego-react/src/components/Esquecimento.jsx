b9e357c5b7a2191d178a090bdefab16adf2721d3
Camilazinha <caamiladepaula03@gmail.com>
2024-10-29 15:54:48 -0300

    Esqueci-minha-senha OK

--- doapego-react/src/components/Esquecimento.jsx
+++ doapego-react/src/components/Esquecimento.jsx
@@ -1,37 +1,47 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import '../styles/main.css';
 import '../styles/layout.css';
 import '../styles/forms.css';
+import '../styles/login.css'
 
-function  Esquecimento () {
+function Esquecimento() {
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate('/login');
+  };
+
   return (
-<div>
-  <div className="container px-4 py-5 bg-white justify-content-center" style={{borderStyle: 'solid', borderColor: '#bbbbbb', borderWidth: 2, borderRadius: 20, width: 400, height: 'auto', boxShadow: '0 0 20px #a0a0a0', marginTop: 30}}>
-    <h2 style={{fontSize: '2em', fontWeight: 600, color: '#FFCF55', textAlign: 'center'}}>Recuperar senha</h2>
-    <form action>
-      <div className="input-box">
-        <span className="icon">
-          <ion-icon name="mail" />
-        </span>
-        <input type="email" placeholder="Email da ONG" required />
-      </div>
-      <p style={{fontSize: '.8em', color: '#4e4e4e', margin: '0 0 0', textAlign: 'center'}}>
-        Insira o email cadastrado para receber o link.
-      </p>
-      <div style={{display: 'flex', justifyContent: 'center'}} className="button-box">
-        <button type="submit" className="mt-3 py-2 btn alt-button" style={{width: '75%', height: 45, background: '#FFA4C5', border: 'none', outline: 'none', borderRadius: 40, cursor: 'pointer', fontSize: '0.9em', color: '#fff', fontWeight: 500, justifyContent: 'center'}}>
-          Enviar link de recuperação
-        </button>
-      </div>
-    </form>
-  </div>
-  <div className="register-link" style={{margin: '-50px 0 0', fontSize: '.9em', color: '#4e4e4e', display: 'flex', justifyContent: 'center'}}>
-    <p><a href="login.html">Voltar para Login</a></p>
-  </div>
-</div>
+    <div>
+      <div className="container px-1 py-5 px-md-4 text-lg-start my-5 borda" style={{ width: 400, height: 'auto' }}>
+        <h2 className='text-center' style={{ fontSize: '2em', fontWeight: 600, color: '#FFCF55' }}>Recuperar senha</h2>
 
-);
+        <form onSubmit={handleSubmit}>
+          <div className="input-box">
+            <span className="icon">
+              <ion-icon name="mail" />
+            </span>
+            <input type="email" placeholder="E-mail" required />
+          </div>
+
+          <p style={{ fontSize: '.8em', color: '#4e4e4e', margin: '0', textAlign: 'center' }}>
+            Insira o email cadastrado para receber o link.
+          </p>
+
+          <div className="button-box dois-links">
+            <button type="submit" className="mt-3 py-2 btn-sm btn-navbar-custom w-75">
+              Enviar link de recuperação
+            </button>
+            <Link to='/login' className='mt-2 py-2 btn-sm text-center w-50 alt-button'>Voltar para o login</Link >
+          </div>
+
+        </form>
+      </div>
+    </div>
+
+  );
 };
 
-  export default Esquecimento;
+export default Esquecimento;
--- doapego-react/src/components/Redefinicao.jsx
+++ doapego-react/src/components/Redefinicao.jsx
@@ -16,6 +16,7 @@ function Redefinicao() {
     <>
       <div className="container px-1 py-5 px-md-4 text-lg-start my-5 borda" style={{ width: 400, height: 'auto' }}>
         <h2 className='text-center' style={{ fontSize: '2em', fontWeight: 600, color: '#FFCF55' }}>Criar nova senha</h2>
+
         <form onSubmit={handleSubmit}>
           <div className="input-box">
             <span className="icon">
@@ -29,6 +30,7 @@ function Redefinicao() {
             </span>
             <input style={{ width: '105%' }} type="password" placeholder="Confirme sua nova senha" required />
           </div>
+
           <p style={{ fontSize: '.8em', color: '#4e4e4e', textAlign: 'center' }}>
             Sua senha deve conter no mínimo 8 caracteres (alfanumérico ou especial)
           </p>
