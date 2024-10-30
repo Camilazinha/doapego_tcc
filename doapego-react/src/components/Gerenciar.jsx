3128dd3d099d6813b5066f6fe23e596926ddb2e2
Camilazinha <caamiladepaula03@gmail.com>
2024-10-30 09:31:52 -0300

    Texto do Login + Gerenciar doações (estrutura)

--- doapego-react/src/components/Gerenciar.jsx
+++ doapego-react/src/components/Gerenciar.jsx
@@ -4,37 +4,38 @@ import '../styles/layout.css';
 import '../styles/forms.css';
 
 
-function Gerenciar () {
+function Gerenciar() {
   return (
 
-<div className="container mt-5">
-  <h2 id="titulo-principal">Gerenciamento de Doações</h2>
-  <div id="cards-container" className="mt-4 row">
-    <div id="card-pendentes" className="card col-md-4 mb-4">
-      <div className="card-body text-center">
-        <h5 className="card-title">Doações Pendentes</h5>
-        <p className="card-text">Veja aqui solicitações de doações que ainda estão aguardando <strong>aprovação</strong>.</p>
-        <a id="link-canceladas" href="#pendentes" className="btn btn-primary">Ver pendentes</a>
+    <div className="container mt-5">
+      <h2 id="titulo-principal">Gerenciamento de Doações</h2>
+      <div id="cards-container2" className="mt-4 row">
+        <div id="card-pendentes" className="card col-md-4 mb-4">
+          <div className="card-body text-center">
+            <h5 className="card-title">Doações Pendentes</h5>
+            <p className="card-text">Veja aqui solicitações de doações que <strong>aguardam aprovação.</strong></p>
+            <a id="link-pendentes" href="pendentes.html" className="btn btn-primary">Ver pendentes</a>
+          </div>
+        </div>
+        <div id="card-feitas" className="card col-md-4 mb-4">
+          <div className="card-body text-center">
+            <h5 className="card-title">Doações Aceitas</h5>
+            <p className="card-text">Veja aqui o histórico de doações <strong>aceitas</strong> e já processadas.</p>
+            <a id="link-feitas" href="feitas.html" className="btn btn-primary">Ver histórico</a>
+          </div>
+        </div>
+        <div id="card-canceladas" className="card col-md-4 mb-4">
+          <div className="card-body text-center">
+            <h5 className="card-title">Doações Recusadas</h5>
+            <p className="card-text">Veja aqui o histórico de doações que foram <strong>recusadas.</strong></p>
+            <a id="link-recusadas" href="canceladas.html" className="btn btn-primary">Ver histórico</a>
+          </div>
+        </div>
       </div>
     </div>
-    <div id="card-feitas" className="card col-md-4 mb-4">
-      <div className="card-body text-center">
-        <h5 className="card-title">Doações Aceitas</h5>
-        <p className="card-text">Veja aqui todas as doações <strong>aceitas</strong> e já processadas.</p>
-        <a id="link-canceladas" href="#feitas" className="btn btn-success">Ver aceitas</a>
-      </div>
-    </div>
-    <div id="card-canceladas" className="card col-md-4 mb-4">
-      <div className="card-body text-center">
-        <h5 className="card-title">Doações Recusadas</h5>
-        <p className="card-text">Veja aqui as doações que foram <strong>recusadas</strong>.</p>
-        <a id="link-canceladas" href="#canceladas" className="btn btn-danger">Ver recusadas</a>
-      </div>
-    </div>
-  </div>
-</div>
 
-);
+
+  );
 };
 
 export default Gerenciar;
