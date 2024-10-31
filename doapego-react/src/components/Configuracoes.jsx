18f64d58180c06c077964d04af5b19ad4fab0c23
Camila <caamiladepaula03@gmail.com>
2024-10-31 10:11:27 -0300

    Responvidade da tela de gerenciar + troca de ícones + opção relativa

--- doapego-react/src/components/Configuracoes.jsx
+++ doapego-react/src/components/Configuracoes.jsx
@@ -5,6 +5,9 @@ import '../styles/layout.css';
 import '../styles/forms.css';
 
 function Configuracoes() {
+  // Recupera o tipo de usuário do localStorage
+  const userType = localStorage.getItem('userType');
+
   return (
     <>
       <div id="profile-container" className="container px-4 py-5 px-sm-5 mt-5 borda">
@@ -14,85 +17,84 @@ function Configuracoes() {
           {/* Editar Perfil */}
           <Link to='/' id="edit-info" className="d-flex align-items-center item-config">
             <span className="icon" id="icon-gerenc">
-              <ion-icon name="pencil-outline" />
+              <ion-icon name="person-circle" />
             </span>
-            <h3 id="section-title">Editar perfil</h3>
-            <span className="arrow-link-container ml-auto">
-              <a href="edit-profile.html" className="arrow-link">
-                <ion-icon name="chevron-forward" />
-              </a>
-            </span>
-          </Link>
-          {/* Segurança */}
-          <Link to='/' id="security-settings" className="d-flex align-items-center item-config">
-            <span className="icon" id="icon-gerenc">
-              <ion-icon name="shield-checkmark" />
-            </span>
-            <h3 id="section-title">
-              Segurança e Privacidade</h3>
-            <span className="arrow-link-container ml-auto">
-              <a href="seguranca.html" className="arrow-link">
-                <ion-icon name="chevron-forward" />
-              </a>
-            </span>
-          </Link>
-          {/* Notificações */}
-          {/* <Link to='/' id="notifications-settings" className="d-flex align-items-center item-config">
-            <span className="icon" id="icon-gerenc">
-              <ion-icon name="notifications" />
-            </span>
-            <h3 id="section-title">
-              Notificações</h3>
-            <span className="arrow-link-container ml-auto">
-              <a href="notif.html" className="arrow-link">
-                <ion-icon name="chevron-forward" />
-              </a>
-            </span>
-          </Link> */}
-          {/* Equipe */}
-          <Link to='/' id="team-settings" className="d-flex align-items-center item-config">
-            <span className="icon" id="icon-gerenc">
-              <ion-icon name="people" />
-            </span>
-            <h3 id="section-title">
-              Equipe</h3>
-            <span className="arrow-link-container ml-auto">
-              <a href="equipe.html" className="arrow-link">
-                <ion-icon name="chevron-forward" />
-              </a>
-            </span>
-          </Link>
-          {/* Histórico de Atividades */}
-          <Link to='/gerenciar' id="activity-history" className="d-flex align-items-center item-config">
-            <span className="icon" id="icon-gerenc">
-              <ion-icon name="document-text" />
-            </span>
-            <h3 id="section-title">
-              Doações recebidas</h3>
-            <span className="arrow-link-container ml-auto">
-              <a href="historico.html" className="arrow-link">
-                <ion-icon name="chevron-forward" />
-              </a>
-            </span>
-          </Link>
-          {/* Ajuda e FAQ */}
-          <Link to="/" id='activity-history' className='d-flex align-items-center item-config'>
-
-            <span className="icon" id="icon-gerenc">
-              <ion-icon name="help-circle" />
-            </span>
-            <h3 id="section-title">
-              Ajuda e FAQ</h3>
+            <h3 id="section-title">Meu perfil</h3>
             <span className="arrow-link-container ml-auto">
               <span className="arrow-link">
                 <ion-icon name="chevron-forward" />
               </span>
             </span>
+          </Link>
 
+          {/* Perfil da Ong */}
+          <Link to='/' id="ong-info" className="d-flex align-items-center item-config">
+            <span className="icon" id="icon-gerenc">
+              <ion-icon name="heart-circle" />
+            </span>
+            <h3 id="section-title">Minha ONG</h3>
+            <span className="arrow-link-container ml-auto">
+              <span className="arrow-link">
+                <ion-icon name="chevron-forward" />
+              </span>
+            </span>
+          </Link>           
+          
+          {/* Equipe - Visível somente para ADMIN_ONG */}
+          {userType === 'ONG' && (
+            <Link to='/' id="team-settings" className="d-flex align-items-center item-config">
+              <span className="icon" id="icon-gerenc">
+                <ion-icon name="people" />
+              </span>
+              <h3 id="section-title">Minha equipe</h3>
+              <span className="arrow-link-container ml-auto">
+                <span className="arrow-link">
+                  <ion-icon name="chevron-forward" />
+                </span>
+              </span>
+            </Link>
+          )}
+
+          {/* Doações recebidas */}
+          <Link to='/gerenciar' id="activity-history" className="d-flex align-items-center item-config">
+            <span className="icon" id="icon-gerenc">
+              <ion-icon name="document-text" />
+            </span>
+            <h3 id="section-title">Doações recebidas</h3>
+            <span className="arrow-link-container ml-auto">
+              <span className="arrow-link">
+                <ion-icon name="chevron-forward" />
+              </span>
+            </span>
+          </Link>
+
+          {/* Segurança */}
+          <Link to='/politica-de-privacidade' id="security-settings" className="d-flex align-items-center item-config">
+            <span className="icon" id="icon-gerenc">
+              <ion-icon name="shield-checkmark" />
+            </span>
+            <h3 id="section-title">Segurança e Privacidade</h3>
+            <span className="arrow-link-container ml-auto">
+              <span className="arrow-link">
+                <ion-icon name="chevron-forward" />
+              </span>
+            </span>
+          </Link>
+
+          {/* Ajuda e FAQ */}
+          <Link to="/" id='activity-history' className='d-flex align-items-center item-config'>
+            <span className="icon" id="icon-gerenc">
+              <ion-icon name="help-circle" />
+            </span>
+            <h3 id="section-title">Ajuda e FAQ</h3>
+            <span className="arrow-link-container ml-auto">
+              <span className="arrow-link">
+                <ion-icon name="chevron-forward" />
+              </span>
+            </span>
           </Link>
         </div>
       </div>
-
     </>
   );
 };
--- doapego-react/src/components/Gerenciar.jsx
+++ doapego-react/src/components/Gerenciar.jsx
@@ -11,21 +11,21 @@ function Gerenciar() {
       <h2 id="titulo-principal">Doações recebidas</h2>
 
       <div id="cards-container2" className="mt-4 row">
-        <div id="card-pendentes" className="card col-md-4 mb-4">
+        <div id="card-pendentes" className="card col-md-6 mb-4">
           <div className="card-body text-center">
             <h5 className="card-title">Doações Pendentes</h5>
             <p className="card-text">Veja aqui solicitações de doações que <strong>aguardam aprovação</strong>.</p>
             <Link to="/" className="btn btn-lg btn-navbar-custom w-75">Ver pendentes</Link>
           </div>
         </div>
-        <div id="card-feitas" className="card col-md-4 mb-4">
+        <div id="card-feitas" className="card col-md-6 mb-4">
           <div className="card-body text-center">
             <h5 className="card-title">Doações Aceitas</h5>
             <p className="card-text">Veja aqui o histórico de doações <strong>aceitas e já processadas</strong>.</p>
             <Link to="/" className="btn btn-lg w-75 btn-navbar-custom">Ver histórico</Link>
           </div>
         </div>
-        <div id="card-canceladas" className="card col-md-4 mb-4">
+        <div id="card-canceladas" className="card col-md-6 mb-4">
           <div className="card-body text-center">
             <h5 className="card-title">Doações Recusadas</h5>
             <p className="card-text">Veja aqui o histórico de doações que foram <strong>recusadas</strong>.</p>
