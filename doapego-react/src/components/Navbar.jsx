58fcc8a0fde328fb7054a0e9f8fe7e3986ff3210
Camila <caamiladepaula03@gmail.com>
2024-10-31 21:57:19 -0300

    navbar funcionante

--- doapego-react/src/components/Navbar.jsx
+++ doapego-react/src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+// src/components/Navbar.js
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -9,59 +11,70 @@ function Navbar({ userType, onLogout }) {
     switch (userType) {
       case 'ONG':
         return (
-<>
-              <li className="nav-item">
-                <Link className="nav-link" to="/">Início</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/parceiros">Parceiros</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/sobre">Propósito</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/tutorial">Como doar</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">Entrar</Link>
-              </li>
-            </>
+          <>
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Início</Link>
+            </li>
+            <li className="nav-item">
+              <button className="nav-link btn" onClick={onLogout}>Logout</button>
+            </li>
+          </>
         );
-        case 'MASTER':
-          return (
-            <>
-              <li className="nav-item">
-                <Link className="nav-link" to="/painel2">Painel Admin 2</Link>
-              </li>
-              <li className="nav-item">
-                <button className="nav-link btn" onClick={onLogout}>Logout</button>
-              </li>
-            </>
-          );
-        case 'FUNCIONARIO_ONG':
-          return (
-            <>
-              <li className="nav-item">
-                <Link className="nav-link" to="/painel3">Painel Admin 3</Link>
-              </li>
-              <li className="nav-item">
-                <button className="nav-link btn" onClick={onLogout}>Logout</button>
-              </li>
-            </>
-          );
-          
-        default:
-          return (
-            <>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">Minha ONG</Link>
-              </li>
-            </>
-          );
-      }
-    };
-         
-    return (
+      case 'MASTER':
+        return (
+          <>
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Início</Link>
+            </li>          
+            <li className="nav-item">
+              <Link className="nav-link" to="/painel2">Painel Admin 2</Link>
+            </li>
+            <li className="nav-item">
+              <button className="nav-link btn" onClick={onLogout}>Logout</button>
+            </li>
+          </>
+        );
+      case 'FUNCIONARIO_ONG':
+        return (
+          <>
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Início</Link>
+            </li>          
+            <li className="nav-item">
+              <Link className="nav-link" to="/painel3">Painel Admin 3</Link>
+            </li>
+            <li className="nav-item">
+              <button className="nav-link btn" onClick={onLogout}>Logout</button>
+            </li>
+          </>
+        );
+      default:
+        return (
+          <>
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Início</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/parceiros">Parceiros</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/sobre">Propósito</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/tutorial">Como doar</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">Entrar</Link>
+            </li>          
+            {/* <li className="nav-item">
+              <Link className="nav-link" to="/login">Minha ONG</Link>
+            </li> */}
+          </>
+        );
+    }
+  };
+
+  return (
     <header>
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid px-2 mx-2">
@@ -72,21 +85,7 @@ function Navbar({ userType, onLogout }) {
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">Início</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/parceiros">Parceiros</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/sobre">Propósito</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/tutorial">Como doar</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">Entrar</Link>
-              </li>
+              {renderMenu()}
             </ul>
             <div className="d-flex ms-auto">
               <a href="https://www.figma.com/proto/bmqmNIZJerSPvmC0WHdVQm/TCC---Prot%C3%B3tipo-Mobile---Doapego?node-id=4-2&t=gtSMir502SSKgJ1x-1&starting-point-node-id=4%3A2&scaling=scale-down content-scaling=fixed" target="_blank" rel="noopener noreferrer" className="btn ml-auto btn-navbar-custom">Baixe o App!</a>
