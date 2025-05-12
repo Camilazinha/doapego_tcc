ed50ec52afb20e6626de6706a500be0ac5ec33a3
Camila de Paula Santana <camila.santana@fieb.edu.br>
2025-05-12 13:35:11 -0300

    Tela fallback OK

--- doapego-react/src/components/NotAuthorized.jsx
+++ doapego-react/src/components/NotAuthorized.jsx
@@ -9,9 +9,9 @@ export default function NotAuthorized() {
       <div className="container my-5">
         <div className="d-grid justify-content-center justify-items-center gap-2" style={{ justifyItems: "center" }}>
           <img src={brokenHeart} style={{ width: "104px", height: "104px" }} alt="coração partido" />
-          <h3>Desculpe! Tela não encontrada.</h3>
+          <h3 className="text-muted fw-bold">Desculpe! Tela não encontrada.</h3>
           <p className="fs-5 text-muted">Se o problema persistir, entre em contato conosco.</p>
-          <Link to=""><button className="mt-3 btn btn-custom-filled">
+          <Link to="/"><button className="mt-3 btn btn-custom-notfound">
             Voltar ao início
           </button></Link>
         </div>
